6135bd0ffe28c72a353ff1fb7a598f0a4c667af6
Shawn <shangbo808@gmail.com>
2019-12-14 11:16:13 +0800

    fix storage adapters bug and add template of storage adapters

---
+++ content/adapters/storage/template.js
@@ -0,0 +1,3 @@
+module.exports = require('ghost-oss-store');
+// custom storage tutorial: https://ghost.org/docs/concepts/storage-adapters/#creating-a-custom-storage-adapter
+// internal ali-oss adapter tutorial: https://github.com/MT-Libraries/ghost-oss-store
